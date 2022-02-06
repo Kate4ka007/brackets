2e485e3889921a1c633805df5c208592f7c931e6
Kate4ka007 <shlemena@mail.ru>
2022-02-07 00:07:48 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,43 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  
+  const openBrackets = ['(', '[', '{', '<' ]
+
+  const pairs = {
+    [')']: '(',
+    [']']: '[',
+    ['}']: '{',
+    ['>']: '<'
+  };
+
+  let stack = [];
+ newStr = str.split('|').join("")
+
+  for(let i = 0; i < newStr.length; i++) {
+
+
+    let current = newStr[i];
+
+
+    if(openBrackets.includes(current)) {
+      stack.push(current);
+    } else {
+      if(stack.length === 0) {
+        return false
+      }
+
+      let topEl = stack[stack.length - 1];
+
+      if (pairs[current] === topEl) {
+        stack.pop()
+      } else {
+        return false;
+      }
+    }
+  }
+if ((str.length - newStr.length) % 2 !== 0) {
+  return false;
+}
+
+return stack.length === 0;
+
 }
